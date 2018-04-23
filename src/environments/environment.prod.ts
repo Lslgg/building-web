4a242f82abbe4a8bf30494b197875619da3c8810
luke <35017119+luke123321@users.noreply.github.com>
2018-04-23 17:32:46 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: true,
-  dataServer: 'http://122.10.93.137:8080'
-  // dataServer: 'http://localhost:8080'
+  production: true,  
+  dataServer: 'http://localhost:8080'
 };
